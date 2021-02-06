585ff8de8c87ee8828a8d52fb35b6e4806a6b0e2
Andrew <Designinglife11@gmail.com>
2021-02-06 07:35:27 -0500

    commit tree 940784424bcd12b3b395d16bddb34f699ca4c06d

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,84 @@
+class TicTacToe
+
+    def initialize
+        @board = Array.new(9, " ")
+    end
+
+    WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} " 
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " 
+    end
+
+    def input_to_index(user_input)
+        user_input.to_i - 1
+    end
+
+    def move(input, move)
+        @board[input] = move
+    end
+
+    def position_taken?(input)
+        @board[input] != " "
+    end
+
+    def valid_move?(input)
+        !position_taken?(input) && input.between?(0, 8)
+    end
+
+    def turn_count
+        @board.count{|position| position != " "}
+    end
+
+    def current_player
+        turn_count.even? ? "X" : "O"
+    end
+
+    def turn
+        user_input = gets.strip
+        input = input_to_index(user_input)
+        if valid_move?(input)
+            position = current_player
+            move(input, position)
+        else
+            turn
+        end
+        display_board
+    end
+
+    def won?
+        WIN_COMBINATIONS.any? do |combo|
+            if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+                return combo
+            end
+        end
+    end
+
+    def full?
+        @board.all?{|position| position != " "}
+    end
+
+    def draw?
+        !won? && full?
+    end
+
+    def over?
+        won? || draw?
+        # won? && draw?
+    end
+
+    def winner
+        if combo = won?
+            @board[combo[0]]
+        end
+    end
+
+    def play
+        turn until over?
+        puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+    end
+end
